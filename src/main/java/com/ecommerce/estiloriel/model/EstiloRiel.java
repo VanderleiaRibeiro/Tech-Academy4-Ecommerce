d80bb7f35a9fecb0b0e090489d2e2e4537a1c7e5
VanderleiaRibeiro <161781397+VanderleiaRibeiro@users.noreply.github.com>
2024-11-17 22:13:07 -0300

    implementando

--- src/main/java/com/ecommerce/estiloriel/model/EstiloRiel.java
+++ src/main/java/com/ecommerce/estiloriel/model/EstiloRiel.java
@@ -1,4 +1,45 @@
 package com.ecommerce.estiloriel.model;
 
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Entity
+@Table
 public class EstiloRiel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String nome;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        EstiloRiel that = (EstiloRiel) o;
+        return Objects.equals(id, that.id) && Objects.equals(nome, that.nome);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, nome);
+    }
 }
